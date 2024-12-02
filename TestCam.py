81b27f1e09231cab8ab59d8797dae0ff540355da
YoneJ <johndang432@gmail.com>
2024-12-02 13:24:24 +0700

    Update TestCam.py

--- TestCam.py
+++ TestCam.py
@@ -2,7 +2,8 @@ import cv2
 cap = cv2.VideoCapture(0)
 ret, frame = cap.read()
 if ret:
-    cv2.imshow('Test Frame', frame)
+    # cv2.imshow('Test Frame', frame)
+    print("success")
     cv2.waitKey(0)
 else:
     print("Error capturing frame")
