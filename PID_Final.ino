e7c7ce50e320be553989838124f93fd1b4a9faec
YoneJ <johndang432@gmail.com>
2024-12-02 17:50:50 +0700

    Update PID_Final.ino

--- PID_Final.ino
+++ PID_Final.ino
@@ -6,21 +6,14 @@ int enR = 13; // enable the right motor driver, using PWM
 int inR1 = 10; // control direction of the right motor 
 int inR2 = 11;
 
-// For Encoder
-int enLA = 2;  // left motor encoder
-int enLB = 3;
-int enRA = 18; // right motor encoder 
-int enRB = 19;
-
 // PID variables
-float Kp = 4.5, Ki = 0.02, Kd = 0.5;
-float sampling_rate = 100; // Adjusted sampling rate
+float Kp = 1.5, Ki = 0.0, Kd = 0.1;  // Adjusted PID constants
 float previousTime;
 float error = 0, P = 0, I = 0, D = 0, PID_value = 0;
 float previous_error = 0;
 
 // Initial motor speed
-int initial_motor_speed = 90;
+int initial_motor_speed = 55;  // Increased initial motor speed
 
 void setup() {
   Serial.begin(9600);
@@ -41,10 +34,16 @@ void setup() {
 
 void loop() {
   if (Serial.available()) {
-    error = Serial.parseFloat();
-    Serial.println(error); // Send the error back to Raspberry Pi
-    calculate_pid(error);
-    motor_control();
+    String message = Serial.readStringUntil('\n');
+    if (message == "stop") {
+      stop();
+      Serial.println("Stopped");
+    } else {
+      error = message.toFloat();
+      Serial.println(error);  // Print the error for debugging
+      calculate_pid(error);
+      motor_control();
+    }
   }
 }
 
@@ -69,10 +68,37 @@ void motor_control() {
   int left_motor_speed = initial_motor_speed + PID_value;
   int right_motor_speed = initial_motor_speed - PID_value;
 
-  // Constrain motor speeds
-  left_motor_speed = constrain(left_motor_speed, 0, 150);
-  right_motor_speed = constrain(right_motor_speed, 0, 150);
+  // Constrain motor speeds 
+  left_motor_speed = constrain(left_motor_speed, 0, 80);
+  right_motor_speed = constrain(right_motor_speed, 10, 80);
 
+  // Control left motor
+  if (left_motor_speed > 0) {
+    digitalWrite(inL1, HIGH);
+    digitalWrite(inL2, LOW);
+  } else if (left_motor_speed < 0) {
+    digitalWrite(inL1, LOW);
+    digitalWrite(inL2, HIGH);
+    left_motor_speed = -left_motor_speed;  // Make the speed positive for PWM
+  } else {
+    digitalWrite(inL1, LOW);
+    digitalWrite(inL2, LOW);
+  }
+
+  // Control right motor
+  if (right_motor_speed > 0) {
+    digitalWrite(inR1, HIGH);
+    digitalWrite(inR2, LOW);
+  } else if (right_motor_speed < 0) {
+    digitalWrite(inR1, LOW);
+    digitalWrite(inR2, HIGH);
+    right_motor_speed = -right_motor_speed;  // Make the speed positive for PWM
+  } else {
+    digitalWrite(inR1, LOW);
+    digitalWrite(inR2, LOW);
+  }
+
+  // Set motor speeds using PWM
   analogWrite(enL, left_motor_speed);  // Set left motor speed
   analogWrite(enR, right_motor_speed); // Set right motor speed
 }
