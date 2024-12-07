a2a8bbb624a220dde8fa9652c16b17d39208f47b
Phuong Anh <119726597+PhuongAnh2212@users.noreply.github.com>
2024-12-07 16:28:45 +0700

    Update test sona and servo connection.ino

--- testing/sketch_dec4a/test sona and servo connection.ino
+++ testing/sketch_dec4a/test sona and servo connection.ino
@@ -1,5 +1,4 @@
 
-
 //        .==.        .==.
 //       //`^\\      //^`\\
 //      // ^ ^\(\__/)/^ ^^\\
@@ -19,17 +18,21 @@ Servo servo;
 // Ý tưởng: cứ có vật thể trước mặt trong khoảng 10cm thì quay servo 180 độ để grab vật thể
 // Câu hỏi: làm sao để giữ nguyên trạng thái của servo? (điều kiện hiện tại là vật thể cách 10cm thì xoay)
 // Khi nào thì thả ra nữa? 
-HCSR04 frontSonar(53, 52);
+HCSR04 frontSonar(51, 50);
 
 float previousTime_sona = 0.0;
 float sampling_rate_sona = 500; //don vi la miliseconds
 
 const int obstacleThreshold = 10; // cm
-const int frontTrigPin = 53;
-const int frontEchoPin = 52;
+const int frontTrigPin = 51;
+const int frontEchoPin = 50;
 
-int pos = 0;    // variable to store the servo position
-
+void setup() {
+  servo.attach(6); // servo pin 6 
+  Serial.begin(9600);
+  pinMode(frontTrigPin, OUTPUT);
+  pinMode(frontEchoPin, INPUT);
+}
 float get_front_distance() {
   float distance = frontSonar.dist();
   if (distance == 0.0) {
@@ -44,12 +47,12 @@ void loop() {
   long distanceFront = get_front_distance();
   Serial.println(distanceFront);
   if (distanceFront < obstacleThreshold) {
-    for (int pos = 0; pos <= 180; pos += 1) { // Goes from 0 degrees to 180 degrees
-      servo.write(pos);              
-      delay(15);                       // Waits 15ms for the servo to reach the position
+    servo.write(180);              
+    delay(5000);                       // Waits 15ms for the servo to reach the position
     }
-  } else {
-      servo.write(0);                  // Reset servo to position 0
+  else {
+    servo.write(90);                  // Reset servo to position 0
+    delay(5000);
   }
 }
 //        .==.        .==.
