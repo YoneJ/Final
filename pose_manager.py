9318fd344e195ff7917e81e2af059aa4e4b1670d
Phuong Anh <119726597+PhuongAnh2212@users.noreply.github.com>
2024-12-12 03:29:44 +0700

    Update pose_manager.py

--- pose_manager.py
+++ pose_manager.py
@@ -1,14 +1,14 @@
 class PoseManager:
     _instance = None
+    _pose = None
 
-    def __new__(cls, *args, **kwargs):
+    def __new__(cls):
         if cls._instance is None:
-            cls._instance = super(PoseManager, cls).__new__(cls, *args, **kwargs)
-            cls._instance.pose = None  # Initialize pose as None
+            cls._instance = super(PoseManager, cls).__new__(cls)
         return cls._instance
 
     def set_pose(self, pose):
-        self.pose = pose
+        self._pose = pose
 
     def get_pose(self):
-        return self.pose
+        return self._pose
